d49dd31b476179b95ca828600c17b7e96767b60b
Strongeric89 <strongeric89@gmail.com>
2016-05-24 17:30:35 +0100

    This is atom commit integration

---
+++ week1/test.java
@@ -0,0 +1,12 @@
+public class test {
+  public static void main(String[] args)
+  {
+    int i ;
+    int j;
+    for(i=0;i<10;i++)
+    {
+    System.out.println("Hello eric!");
+  }
+  }//end main()
+
+}//end class
