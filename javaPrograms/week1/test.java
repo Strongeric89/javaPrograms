ff46ccc7191efcf82471cc7645ffbf726c30ff6b
Strongeric89 <ericstrong89@gmail.com>
2016-05-25 12:03:47 +0100

    ERIC STRONG

--- javaPrograms/week1/test.java
+++ javaPrograms/week1/test.java
@@ -1,7 +1,8 @@
 public class test {
   public static void main(String[] args)
   {
-    int i ;//fdfddfdfcvcv
+    int i ;//fdfddfdfcvcvdfgfgfg
+    int j; //eric strong
 
     for(i=0;i<10;i++)
     {
