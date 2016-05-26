bfa1d0dbf34ef5b60ed8452901a71cf477b7c0c2
Strongeric89 <ericstrong89@gmail.com>
2016-05-26 19:34:07 +0100

    arrays and 2d arrays

---
+++ study/letters.java
@@ -0,0 +1,28 @@
+/*Print out the array of letters backwards*/
+import java.util.Scanner;
+public class letters{
+  public static void main(String [] args)
+  {
+    Scanner scanf = new Scanner (System.in);
+    char letters[]= {'E','R','I','C'};
+
+    for(int i = letters.length-1; i>-1;i--)
+    {
+      System.out.print(letters[i]);
+    }//end for
+
+  //printing out an array of integers
+  int dob[]={2,3,1989};
+  for(int i=0;i<dob.length;i++)
+  {
+    System.out.print(dob[i]+ "/");
+  }//end for
+
+  int a1[];
+  System.out.println("enter numbers to array:");
+  for(int i=0;i<a1.length;i++)
+  {
+    
+  }//end for
+  }//end main
+}//end class
