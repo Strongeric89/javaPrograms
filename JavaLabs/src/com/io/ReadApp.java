b00823b06587e892ff47bbcc5c29adaeb00ed701
Strongeric89 <ericstrong89@gmail.com>
2017-03-02 16:00:49 +0000

    lab 6

--- JavaLabs/src/com/io/ReadApp.java
+++ JavaLabs/src/com/io/ReadApp.java
@@ -2,35 +2,61 @@ package com.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
+/**
+ * @author Eric Strong Date: 23 Feb 2017 GoogleHashCode: file reading class ID:
+ *         C15708709
+ */
 public class ReadApp
 {
 	public static void main(String[] args) throws IOException
 	{
-		String filename = "C:\\Users\\Eric\\Documents\\GitHub\\javaPrograms\\JavaLabs\\src\\com\\io\\test.txt";
+		// enter file name here
+		String filename = "me_at_the_zoo.in";
 		File file = new File(filename);
+		String lineOne = "";
 		Scanner in = new Scanner(file);
 		int i = 0;
-		String[] lines = new String[10];
+
 		while (in.hasNextLine())
 		{
-			String line = (i + 1) + ".\t" + in.nextLine() + "\t ADDING IN TEXT";
-			lines[i] = line;
+
+			String line = (i + 1) + ".\t" + in.nextLine();
 			System.out.println(line);
+
+			if (i == 0)
+			{
+				lineOne = line;
+			} // end if
 			i++;
+
 		}
 
-		// writing to a file
-		String text = "";
-		for (int j = 0; j < i; j++)
+		// int numbers[] = Integer.parseInt(lineOne.split(" "));
+		System.out.println(lineOne);
+		String numbers[] = lineOne.split(" ");
+
+		int v = 0;
+		int realNumbers[] = new int[4];
+		int k = 0;
+		for (String num : numbers)
 		{
-			text = lines[j];
-			// saves file to the parent directory of the project - f5 to refresh
-			Files.write(Paths.get("./fileName.txt"), text.getBytes());
+			System.out.println(num);
+			// realNumbers[k] = Integer.parseInt(num);
+			// // realNumbers[k] = v;
+			// k++;
 		}
 
+		in.close();
 	}// end main
-}// end class
+}// end ReadApp Class
+
+// // writing to a file
+// String text = "";
+// for (int j = 0; j < i; j++)
+// {
+// text = lines[j];
+// // saves file to the parent directory of the project - f5 to refresh
+// Files.write(Paths.get("./fileName.txt"), text.getBytes());
+// }
